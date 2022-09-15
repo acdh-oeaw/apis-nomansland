9bd5e4854dbda1621602774214d01b26d4fc9ec7
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2022-09-15 09:23:00 +0200

    bind to port 5000

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 #FIXME: remove that
 #sed -i '65 s/value.id = value.text;/value.id = value.id;/' /app/.heroku/python/lib/python3.8/site-packages/dal_select2/static/autocomplete_light/select2.js
-echo "$DJANGO_SETTINGS_MODULE"
-echo "$DATABASE_URL"
 python manage.py collectstatic --noinput --settings=apis.settings.dev
 python manage.py migrate --settings=apis.settings.dev
-gunicorn apis.wsgi --timeout 120 --workers=3 --threads=3 --worker-connections=1000
+gunicorn apis.wsgi -b 0.0.0.0:5000 --timeout 120 --workers=3 --threads=3 --worker-connections=1000
