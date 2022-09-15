8c3377fa6f72ad0f6ca97151e392d95f1dd02bf5
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2022-09-15 09:46:28 +0200

    adding sentry

--- apis/settings/dev.py
+++ apis/settings/dev.py
@@ -259,3 +259,20 @@ APIS_ENTITIES = {
         ],
     },
 }
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://e41265a66f25498198c3cc9248620fef@sentry.acdh-dev.oeaw.ac.at/13",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
